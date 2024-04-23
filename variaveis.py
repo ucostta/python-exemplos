fb6c54ab19848e89a2e7ef4c343ea70d25c72391
ucostta <gustavofixo04@gmail.com>
2024-04-23 16:38:24 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Gustavo Da Costa"
+idade=16
+email='gustavocsenai2024@gmail.com'
+telefone='17996240321'
+nota=10
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
