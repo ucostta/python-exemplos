72693bb0938484c8eb1bb8eb62ac0062684fe005
ucostta <gustavofixo04@gmail.com>
2024-05-02 16:44:36 -0300

    Update fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola = 'Senai'
 curso = 'Desenvolvimento de Sistemas'
 uc = 'Lógica de Programção'
+matricula = 34
+nota = 10
 print(f"Escola: {escola}\n"
       f"Curso: {curso}\n"
-      f"Unidade Curricular: {uc}")
+      f"Unidade Curricular: {uc}"
+      f"Matricula: {matricula:06d}"
+      f"Nota: {nota:.2f}")
