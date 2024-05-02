a30555434d25bf11ed3571f02a7b2f58e500fbb0
ucostta <gustavofixo04@gmail.com>
2024-05-02 16:30:34 -0300

    criação fstrings2.py

---
+++ fstrings2.py
@@ -0,0 +1,7 @@
+#Formatação de Strings com várias linhas
+escola = 'Senai'
+curso = 'Desenvolvimento de Sistemas'
+uc = 'Lógica de Programção'
+print(f"Escola: {escola}\n"
+      f"Curso: {curso}\n"
+      f"Unidade Curricular: {uc}")
