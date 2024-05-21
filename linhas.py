7c3a5ae473da94c7ae21b04a18f0964390b4bf41
ucostta <gustavofixo04@gmail.com>
2024-05-21 14:55:06 -0300

    Create linhas.py

---
+++ linhas.py
@@ -0,0 +1,8 @@
+aluno = ["Renan Silva", 4, 8.5, True]
+print(f"Nome: {aluno[0]}")
+print(f"Faltas: {aluno[1]}")
+print(f"Média: {aluno[2]}")
+if aluno[3] == True:
+    print(f"Aprovado: Sim")
+else:
+    print(f"Aprovado:Não")
