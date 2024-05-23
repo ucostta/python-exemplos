2d4312acf7768c35661407b17028a7bfcce08142
ucostta <gustavofixo04@gmail.com>
2024-05-23 09:35:02 -0300

    Create listas_remover.py

---
+++ listas_remover.py
@@ -0,0 +1,11 @@
+alunos=['Viktor', 'Guizin', 'Carlos', 'Nobru', 'Marcos', 'Viktor']
+print(f"Lista de alunos: {alunos}")
+# Método remove()
+alunos.remove('Viktor')
+print(f"Lista de alunos: {alunos}")
+# Método pop()
+alunos.pop(2)
+print(f"Lista de alunos: {alunos}")
+# Função del
+del alunos[3]
+print(f"Lista de alunos: {alunos}")
