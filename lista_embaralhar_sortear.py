7849f95c6fd08fcd319f899c3da7ac62cc7acb1d
ucostta <gustavofixo04@gmail.com>
2024-05-23 08:53:16 -0300

    Create lista_embaralhar_sortear.py

---
+++ lista_embaralhar_sortear.py
@@ -0,0 +1,9 @@
+import random
+alunos=['João', 'Maria', 'Pedro', 'Ana', 'Lucas', 'Mariana']
+print(f"Lista: {alunos}")
+# Embaralhar a Lista
+random.shuffle(alunos)
+print(f"Lista embaralhada: {alunos}")
+# Escolhe um aluno aleatoriamente
+aluno_sorteado = random.choice(alunos)
+print(f"Aluno sorteado: {aluno_sorteado}")
